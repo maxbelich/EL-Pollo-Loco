b6038af7da1ef4b9e080f4de28eff431b796ba4b
Max Belich <244745219+maxbelich@users.noreply.github.com>
2026-06-29 17:36:01 +0200

    Feat: added chicken kill logic

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,10 +3,15 @@ class Chicken extends MovableObject {
   y = 345;
   width = 80;
   height = 80;
+  isDead = false;
   IMAGES_WALKING = [
     "assets/imgs/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
     "assets/imgs/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
     "assets/imgs/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
+
+  IMAGES_DEAD = [
+    "assets/imgs/3_enemies_chicken/chicken_normal/2_dead/dead.png",
   ];
 
   constructor() {
@@ -16,16 +21,25 @@ class Chicken extends MovableObject {
     );
     this.speed = 0.6 + Math.random() * 0.4;
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_DEAD);
     this.animate();
   }
 
   animate() {
     setInterval(() => {
-      this.moveLeft();
+      if (this.isDead) {
+        this.img = this.imageCache[this.IMAGES_DEAD[0]];
+      } else {
+        this.moveLeft();
+        this.playAnimation(this.IMAGES_WALKING);
+      }
     }, 1000 / 60);
+  }
 
-    setInterval(() => {
-      this.playAnimation(this.IMAGES_WALKING);
-    }, 115);
+  hitFromAbove() {
+    if (!this.isDead) {
+      this.isDead = true;
+      this.img = this.imageCache[this.IMAGES_DEAD[0]];
+    }
   }
 }
